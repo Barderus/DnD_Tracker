f3489770ce610797882f7925403dbced834d4572
Barderus <reis.sev@gmail.com>
2026-05-21 17:31:55 -0500

    Add account creation scaffolding

---
+++ account/acc_creation.py
@@ -0,0 +1,25 @@
+import hashlib
+import secrets
+
+def hash_password(password):
+    pass
+
+def verify_password(password, stored_password):
+    pass
+
+
+def create_account(username, password):
+    pass
+
+def main():
+    username = input("Username: ")
+    password = input("Password: ")
+
+    # Check if the username + password combination already exists
+
+    fname = input("First name: ")
+    lname = input("Last name: ")
+
+
+if __name__ == "__main__":
+    main()
